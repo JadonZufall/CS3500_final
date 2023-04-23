a446523d92aa98818fdb9061b598953f7dc09270
Jadon <jadonzufall@gmail.com>
2023-04-23 14:29:02 -0400

    auth features

--- views/index.php
+++ views/index.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
 if ($_SESSION['loggedin']) {
-    echo 'logged in as ' . $_SESSION['name'];
+    echo '<div class="container auththing">';
+    echo 'logged in as ' . $_SESSION['name'] . ' ';
+    echo '<a href="/signout">signout</a>';
+    echo '</div>';
 }
 else {
-    echo 'currently signed out';
+    echo '<div class="container auththing">';
+    echo 'currently signed out ';
+    echo '<a href="/login">login</a> | <a href="/signup">signup</a>';
+    echo '</div>';
 }
 require('inc/header.inc');
 include('static/index.html');

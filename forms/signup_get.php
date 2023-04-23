3caefc03f0a689991532abd28c7963342de4da4f
Jadon <jadonzufall@gmail.com>
2023-04-23 13:08:01 -0400

    documentation

--- forms/signup_get.php
+++ forms/signup_get.php
@@ -11,6 +11,5 @@
 <form action="forms/register_post.php" method="post">
     <input type="text" name="user"></input><br>
     <input type="password" name="pass"></input><br>
-    <input type="password" name="validation"></input><br>
     <input type="submit"><br>
 </form>
--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,13 +1,22 @@
 <?php
+/* 
+    Include to connect to database from anywhere in the server I guess?
+    Times like this I wish we could use django or something but I guess
+    php is fun.
+*/
+
+// Database config
 $servername = 'localhost';
 $username = "server";
 $password = "password";
 $dbname = "taylor_swift";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     echo "Error unable to connect to database";
     die("Database connection failed " . $conn->connect_error);
 }
 
+// Set global variable? might be doing this incorrectly.
 $GLOBALS['conn'] = conn;
